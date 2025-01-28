94e0dd4754a3900a377e9709883171f2bcf38577
Niklas Wockenfuß <nikwo97@aim.com>
2025-01-28 21:56:17 +0100

    Remove unused build commands and delete outdated documentation

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 (cd ros_pcl_module && rm -rf build/ install/ log/ && colcon build)
-(cd ros_zivid_driver && rm -rf build/ install/ log/ && colcon build)
-# (cd ros_open3d_module && rm -rf build/ install/ log/ && colcon build)
